7e485f7ee85b33ddd4a77c3ecd8c3eb79b4e0dcc
MelonGO <cjwone1@163.com>
2015-03-08 19:28:55 +0800

    增加了一些图片资源

--- src/main/java/presentation/mainui/MainFrame.java
+++ src/main/java/presentation/mainui/MainFrame.java
@@ -44,16 +44,8 @@ public class MainFrame {
 			UIManager.setLookAndFeel(new SubstanceLookAndFeel());      
             if (System.getProperty("substancelaf.useDecorations") == null) {      
                 JFrame.setDefaultLookAndFeelDecorated(true);      
-                /**  
-                 * setDefaultLookAndFeelDecorated设置为true或者false    
-                 * 区别如 图2—setDefaultLookAndFeelDecorated 所示    
-                 * JDialog也可以使用setDefaultLookAndFeelDecorated    
-                 */     
-            }      
-            /**  
-             * 设置全局字体样式，否则中文将产生乱码    
-             * initGlobalFontSetting是自定义的方法，在上方    
-             */    
+                
+            }
             // 设置当前的主题风格，同样我 们还可以设置当前的按钮形状，水印风格等等      
             SubstanceLookAndFeel.setCurrentTheme(new SubstanceAquaTheme());
 			} catch (Exception e){
@@ -87,7 +79,7 @@ public class MainFrame {
 	 */
 	private void initialize() {
 		frmNba = new JFrame();
-		frmNba.setIconImage(Toolkit.getDefaultToolkit().getImage("D:\\pictures\\NBA.jpg"));
+		frmNba.setIconImage(Toolkit.getDefaultToolkit().getImage("pictures\\NBA.jpg"));
 		frmNba.setTitle("NBA");
 		frmNba.setBounds(100, 100, 1000, 900);
 		frmNba.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -100,7 +92,7 @@ public class MainFrame {
 		panel.setLayout(null);
 		
 		lblNewLabel_6 = new JLabel("");
-		lblNewLabel_6.setIcon(new ImageIcon("D:\\pictures\\lakers.jpg"));
+		lblNewLabel_6.setIcon(new ImageIcon("pictures\\lakers.jpg"));
 		lblNewLabel_6.setBounds(400, 0, 600, 200);
 		panel.add(lblNewLabel_6);
 		
@@ -110,7 +102,7 @@ public class MainFrame {
 		panel_1.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon("D:\\pictures\\up2.jpg"));
+		lblNewLabel.setIcon(new ImageIcon("pictures\\middle.jpg"));
 		lblNewLabel.setBounds(0, 0, 1000, 100);
 		panel_1.add(lblNewLabel);
 		
@@ -120,48 +112,48 @@ public class MainFrame {
 		panel_2.setLayout(null);
 		
 		JLabel lblNewLabel_1 = new JLabel("");
-		lblNewLabel_1.setIcon(new ImageIcon("D:\\pictures\\CHI2.jpg"));
+		lblNewLabel_1.setIcon(new ImageIcon("pictures\\CHI.jpg"));
 		lblNewLabel_1.setBounds(10, 10, 150, 100);
 		panel_2.add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("");
-		lblNewLabel_2.setIcon(new ImageIcon("D:\\pictures\\CLE2.jpg"));
+		lblNewLabel_2.setIcon(new ImageIcon("pictures\\CLE.jpg"));
 		lblNewLabel_2.setBounds(10, 120, 150, 100);
 		panel_2.add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_3 = new JLabel("");
-		lblNewLabel_3.setIcon(new ImageIcon("D:\\pictures\\DET2.jpg"));
+		lblNewLabel_3.setIcon(new ImageIcon("pictures\\DET.jpg"));
 		lblNewLabel_3.setBounds(10, 230, 150, 100);
 		panel_2.add(lblNewLabel_3);
 		
 		JLabel lblNewLabel_4 = new JLabel("");
-		lblNewLabel_4.setIcon(new ImageIcon("D:\\pictures\\IND2.jpg"));
+		lblNewLabel_4.setIcon(new ImageIcon("pictures\\IND.jpg"));
 		lblNewLabel_4.setBounds(10, 340, 150, 100);
 		panel_2.add(lblNewLabel_4);
 		
 		JLabel lblNewLabel_5 = new JLabel("");
-		lblNewLabel_5.setIcon(new ImageIcon("D:\\pictures\\MIL2.jpg"));
+		lblNewLabel_5.setIcon(new ImageIcon("pictures\\MIL.jpg"));
 		lblNewLabel_5.setBounds(10, 450, 150, 100);
 		panel_2.add(lblNewLabel_5);
 	}
 	
 	private class ScheduleTask extends TimerTask {
         public void run() {
-        	lblNewLabel_6.setIcon(new ImageIcon("D:\\pictures\\lakers.jpg"));
+        	lblNewLabel_6.setIcon(new ImageIcon("pictures\\lakers.jpg"));
         	try {
         		Thread.sleep(3000);
         		} 
         	catch (InterruptedException e) {
         		e.printStackTrace();
         	}
-        	lblNewLabel_6.setIcon(new ImageIcon("D:\\pictures\\ROYALTY2.jpg"));
+        	lblNewLabel_6.setIcon(new ImageIcon("pictures\\ROYALTY.jpg"));
         	try {
         		Thread.sleep(3000);
         		} 
         	catch (InterruptedException e) {
         		e.printStackTrace();
         	}
-        	lblNewLabel_6.setIcon(new ImageIcon("D:\\pictures\\WADE2.jpg"));
+        	lblNewLabel_6.setIcon(new ImageIcon("pictures\\WADE2.jpg"));
         	try {
         		Thread.sleep(3000);
         		} 

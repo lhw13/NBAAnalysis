374ee16cd042d5d3df90e8d00218a53606d75af8
lhw13 <1413246981@qq.com>
2015-03-10 19:54:50 +0800

    增加了逻辑层的桩程序 by lhw

--- src/main/java/server/businesslogic/Compute.java
+++ src/main/java/server/businesslogic/Compute.java
@@ -1,5 +1,34 @@
 package server.businesslogic;
 
-public class Compute {
+import java.util.ArrayList;
 
+import vo.PlayerVO;
+import vo.TeamVO;
+import blservice.BLService;
+
+public class Compute implements BLService{
+	private Compute(){}
+	private static Compute instance = null;
+	public static Compute getInstance()
+	{
+		if(instance == null)
+			instance = new Compute();
+		return instance;
+	}
+	public ArrayList<PlayerVO> getPlayerAnalysis()
+	{
+		return new ArrayList();
+	}
+	public ArrayList<TeamVO> getTeamAnalysis()
+	{
+		return new ArrayList();
+	}
+	public PlayerVO getPlayerAnalysis(String name)
+	{//暂时认为名字唯一确定一名球员
+		return new PlayerVO();
+	}
+	public TeamVO getTeamAnalysis(String name)
+	{//暂时认为球队全称唯一确定一支球队
+		return new TeamVO();
+	}
 }

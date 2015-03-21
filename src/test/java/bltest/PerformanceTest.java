c22dce259c7c8e7dd2dc957e5f97adcf4a0f4cea
lhw13 <1413246981@qq.com>
2015-03-21 11:10:36 +0800

    增加了性能测试 by lhw

--- src/test/java/bltest/PerformanceTest.java
+++ src/test/java/bltest/PerformanceTest.java
@@ -1,11 +1,28 @@
 package bltest;
 
-import org.junit.Test;
+import org.junit.*;
+
+import blservice.BLService;
+import server.businesslogic.BLController;
 
 public class PerformanceTest {
-	@Test
-	public void testPerformance()
+	@Ignore
+	public void testPerformanceWithCycle()
 	{
-		
+		BLService bl = BLController.getInstance();
+		long begin = System.currentTimeMillis();
+		for(int i=0;i<6400;i++)
+			bl.getPlayerAnalysis();
+		long end = System.currentTimeMillis();
+		System.out.println(end-begin);
+	}
+	@Test
+	public void testPerformanceWithMatches()
+	{
+		BLService bl = BLController.getInstance();
+		long begin = System.currentTimeMillis();
+		bl.getPlayerAnalysis();
+		long end = System.currentTimeMillis();
+		System.out.println(end-begin);
 	}
 }

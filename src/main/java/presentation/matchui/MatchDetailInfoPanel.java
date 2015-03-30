1d1b4f59dc32f57382f40168fe0ef1ad9753ba82
majormark <406596594@qq.com>
2015-03-30 21:35:49 +0800

    增加了比赛详细信息面板

---
+++ src/main/java/presentation/matchui/MatchDetailInfoPanel.java
@@ -0,0 +1,64 @@
+package presentation.matchui;
+
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class MatchDetailInfoPanel extends JPanel {
+	public static JScrollPane scrollPane;
+	JPanel panelOfBottom = new JPanel();
+	private JTable table;
+	private JScrollPane scrollPane_2;
+	private JTable table_1;
+	
+	public MatchDetailInfoPanel() {
+		this.setBounds(0, 0, 1000, 600);
+		setLayout(null);
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setRowHeaderView(panelOfBottom);
+		scrollPane.setBounds(0, 0, 990, 600);
+		scrollPane.getVerticalScrollBar().setUnitIncrement(20);
+		add(scrollPane);
+		
+		panelOfBottom.setPreferredSize(new Dimension(1000, 600));
+		panelOfBottom.setLayout(null);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(66, 155, 720, 199);
+		panelOfBottom.add(scrollPane_1);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+			},
+			new String[] {
+				"New column", "New column", "New column", "New column", "New column", "New column"
+			}
+		));
+		scrollPane_1.setViewportView(table);
+		
+		scrollPane_2 = new JScrollPane();
+		scrollPane_2.setBounds(66, 427, 720, 163);
+		panelOfBottom.add(scrollPane_2);
+		
+		table_1 = new JTable();
+		table_1.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+			},
+			new String[] {
+				"New column", "New column", "New column", "New column", "New column"
+			}
+		));
+		scrollPane_2.setViewportView(table_1);
+	}
+
+}

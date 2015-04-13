4f074d11c74fad0b958984497dff2ad811e3d346
lhw13 <1413246981@qq.com>
2015-04-13 19:16:18 +0800

    增加一个性能测试，若干正确测试 by lhw

--- src/test/java/consoletest/ConsoleTest.java
+++ src/test/java/consoletest/ConsoleTest.java
@@ -1,5 +1,9 @@
 package consoletest;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -12,5 +16,85 @@ public class ConsoleTest {
 		Console c = new Console();
 		String ss[] = {"-player","-hot","assist","-n","5"};
 		c.execute(System.out, ss);
+	}
+	
+	@Test
+	public void testTeam1()
+	{
+		Console c = new Console();
+		String ss[] = {"-team"};
+		PrintStream p = null;
+		try {
+			p = new PrintStream(new File("test1.txt"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		c.execute(p, ss);
+	}
+	
+	@Test
+	public void testTeam2()
+	{
+		Console c = new Console();
+		String ss[] = {"-team","hot","assist","-n","5"};
+		PrintStream p = null;
+		try {
+			p = new PrintStream(new File("test2.txt"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		c.execute(p, ss);
+	}
+	
+	@Test
+	public void testTeam3()
+	{
+		Console c = new Console();
+		String ss[] = {"-team","-total","-all","-n","10","-sort","shot.desc"};
+		PrintStream p = null;
+		try {
+			p = new PrintStream(new File("test3.txt"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		c.execute(p, ss);
+	}
+	
+	@Test
+	public void testTeam4()
+	{
+		Console c = new Console();
+		String ss[] = {"-team","-high","-n","5","-sort","stealEfficient.asc"};
+		PrintStream p = null;
+		try {
+			p = new PrintStream(new File("test4.txt"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		c.execute(p, ss);
+	}
+	
+	@Test
+	public void testTeamPerformanceStatic()
+	{
+		String ss[] = {"-player","-hot","assist","-n","5"};
+		String ss2[] = {"-player"};
+		Console c = new Console();
+		PrintStream p = null;
+		try {
+			p = new PrintStream(new File("testPerformance.txt"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		for(int i=0;i<10;i++)
+		{
+			c.execute(p, ss);
+			c.execute(p, ss2);
+		}
 	}
 }

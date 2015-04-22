8c2b422a11cde2dec8249b517337ee4e28ee9a00
Majormark <Majormark@major>
2015-04-22 16:48:04 +0800

    走你

--- src/main/java/presentation/mainui/MainFrame.java
+++ src/main/java/presentation/mainui/MainFrame.java
@@ -762,6 +762,7 @@ public class MainFrame {
 					
 					MainFrame.panel.setVisible(false);
 					frame.getContentPane().add(hrp.scrollPane);
+					backPanels.add(currentPanel);
 					currentPanel = Panels.HotRankingPanel;
 					hrp.update1();	
 					hrp.update2();

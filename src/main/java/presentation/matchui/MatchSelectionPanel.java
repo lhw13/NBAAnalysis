92e5ff02f821afa5ac2c4420459fc47efdc716a0
MelonGO <cjwone1@163.com>
2015-04-22 14:59:49 +0800

    ~~~~

--- src/main/java/presentation/matchui/MatchSelectionPanel.java
+++ src/main/java/presentation/matchui/MatchSelectionPanel.java
@@ -406,6 +406,7 @@ public class MatchSelectionPanel extends JPanel {
 			MainFrame.frame.getContentPane().add(mdip.scrollPane);
 			MainFrame.frame.repaint();//刷新重画 
 			MainFrame.frame.validate();//保证重画后的窗口能正常立即显示 
+			MainFrame.currentPanel = Panels.MatchDetailInfoPanel;
 		}
 	}
 	
@@ -602,6 +603,7 @@ public class MatchSelectionPanel extends JPanel {
 			MainFrame.frame.getContentPane().add(mdip.scrollPane);
 			MainFrame.frame.repaint();//刷新重画 
 			MainFrame.frame.validate();//保证重画后的窗口能正常立即显示 
+			MainFrame.currentPanel = Panels.MatchDetailInfoPanel;
 		}
 		
 	}

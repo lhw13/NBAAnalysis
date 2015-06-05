c1b43cfaaf40481bfe3a8a9e450bb9e871f0586e
MelonGO <cjwone1@163.com>
2015-06-05 16:48:50 +0800

    球员对比图表已完成

--- src/main/java/presentation/playerui/PlayerComparePanel.java
+++ src/main/java/presentation/playerui/PlayerComparePanel.java
@@ -11,10 +11,12 @@ import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Vector;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -89,6 +91,18 @@ public class PlayerComparePanel extends JPanel {
 	public static String player_1 = "NoPlayer1";
 	public static String player_2 = "NoPlayer2";
 
+	private static JCheckBox CheckBox_1;
+	private static JCheckBox CheckBox_2;
+	private static JCheckBox CheckBox_3;
+	private static JCheckBox CheckBox_4;
+	private static JCheckBox CheckBox_5;
+	private static JCheckBox CheckBox_6;
+	private static JCheckBox CheckBox_7;
+	private static JCheckBox CheckBox_8;
+
+	private static ArrayList<String> checkBoxItem = new  ArrayList<String>();
+	private static HashMap<String,int[]> dataMap = new HashMap<String,int[]>();
+
 	public PlayerComparePanel() {
 		this.setBounds(0, 0, 1000, 600);
 		setLayout(null);
@@ -107,6 +121,101 @@ public class PlayerComparePanel extends JPanel {
 		//		charPanel.setBounds(50, 250, 900, 300);
 		//		charPanel.setVisible(true);
 		//		panelOfBottom.add(charPanel);
+
+		//checkbox=====================================================
+		checkBoxItem.add("得分");
+		checkBoxItem.add("篮板");
+		checkBoxItem.add("助攻");
+		checkBoxItem.add("罚球");
+		checkBoxItem.add("抢断");
+
+		CheckBox_1 = new JCheckBox("得分");
+		CheckBox_1.setBounds(10, 250, 100, 30);
+		panelOfBottom.add(CheckBox_1);
+		CheckBox_1.setSelected(true);
+		CheckBox_1.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				changeTheChart(1);
+				update_chart(player_1, player_2);
+			}
+		});
+
+		CheckBox_2 = new JCheckBox("篮板");
+		CheckBox_2.setBounds(110, 250, 100, 30);
+		panelOfBottom.add(CheckBox_2);
+		CheckBox_2.setSelected(true);
+		CheckBox_2.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				changeTheChart(2);
+				update_chart(player_1, player_2);
+			}
+		});
+
+		CheckBox_3 = new JCheckBox("助攻");
+		CheckBox_3.setBounds(210, 250, 100, 30);
+		panelOfBottom.add(CheckBox_3);
+		CheckBox_3.setSelected(true);
+		CheckBox_3.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				changeTheChart(3);
+				update_chart(player_1, player_2);
+			}
+		});
+
+		CheckBox_4 = new JCheckBox("罚球");
+		CheckBox_4.setBounds(310, 250, 100, 30);
+		panelOfBottom.add(CheckBox_4);
+		CheckBox_4.setSelected(true);
+		CheckBox_4.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				changeTheChart(4);
+				update_chart(player_1, player_2);
+			}
+		});
+
+		CheckBox_5 = new JCheckBox("抢断");
+		CheckBox_5.setBounds(600, 250, 100, 30);
+		panelOfBottom.add(CheckBox_5);
+		CheckBox_5.setSelected(true);
+		CheckBox_5.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				changeTheChart(5);
+				update_chart(player_1, player_2);
+			}
+		});
+
+		CheckBox_6 = new JCheckBox("盖帽");
+		CheckBox_6.setBounds(710, 250, 100, 30);
+		panelOfBottom.add(CheckBox_6);
+		CheckBox_6.setEnabled(false);
+		CheckBox_6.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				changeTheChart(6);
+				update_chart(player_1, player_2);
+			}
+		});
+
+		CheckBox_7 = new JCheckBox("失误");
+		CheckBox_7.setBounds(810, 250, 100, 30);
+		panelOfBottom.add(CheckBox_7);
+		CheckBox_7.setEnabled(false);
+		CheckBox_7.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				changeTheChart(7);
+				update_chart(player_1, player_2);
+			}
+		});
+
+		CheckBox_8 = new JCheckBox("犯规");
+		CheckBox_8.setBounds(910, 250, 100, 30);
+		panelOfBottom.add(CheckBox_8);
+		CheckBox_8.setEnabled(false);
+		CheckBox_8.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				changeTheChart(8);
+				update_chart(player_1, player_2);
+			}
+		});
 
 		//label =======================================================
 		labelOfPhoto1 = new JLabel("");
@@ -437,25 +546,51 @@ public class PlayerComparePanel extends JPanel {
 		if(!playerName2.equals("NoPlayer2")){
 			PlayerVO vo1 = blservice.getPlayerAnalysis(playerName1);
 			PlayerVO vo2 = blservice.getPlayerAnalysis(playerName2);
-			int assist_1 = vo1.getAssist();
-			int assist_2 = vo2.getAssist();
-			int score_1 = vo1.getScore();
-			int score_2 = vo2.getScore();
-			int steal_1 = vo1.getSteal();
-			int steal_2 = vo2.getSteal();
-			int miss_1 = vo1.getMiss();
-			int miss_2 = vo2.getMiss();
-			int block_1 = vo1.getBlock();
-			int block_2 = vo2.getBlock();
-			int[] dataArray = {assist_1,assist_2,score_1,score_2,
-					steal_1,steal_2,miss_1,miss_2,block_1,block_1};
+
+			for(int i=0;i<checkBoxItem.size();i++){
+				switch(checkBoxItem.get(i)){
+				case "得分":
+					int[] scoreArray = {vo1.getScore(), vo2.getScore()};
+					dataMap.put("得分", scoreArray);
+					break;
+				case "篮板":
+					int[] reboundArray = {vo1.getTotalRebound(), vo2.getTotalRebound()};
+					dataMap.put("篮板", reboundArray);
+					break;
+				case "助攻":
+					int[] assistArray = {vo1.getAssist(), vo2.getAssist()};
+					dataMap.put("助攻", assistArray);
+					break;
+				case "罚球":
+					int[] freehitArray = {vo1.getFreeHit(), vo2.getFreeHit()};
+					dataMap.put("罚球", freehitArray);
+					break;
+				case "抢断":
+					int[] stealArray = {vo1.getSteal(), vo2.getSteal()};
+					dataMap.put("抢断", stealArray);
+					break;
+				case "盖帽":
+					int[] blockArray = {vo1.getBlock(), vo2.getBlock()};
+					dataMap.put("盖帽", blockArray);
+					break;
+				case "失误":
+					int[] missArray = {vo1.getMiss(), vo2.getMiss()};
+					dataMap.put("失误", missArray);
+					break;
+				case "犯规":
+					int[] foulArray = {vo1.getFoul(), vo2.getFoul()};
+					dataMap.put("犯规", foulArray);
+					break;
+				}
+			}
+			
 			String[] players = {playerName1, playerName2};
 
 			if(charPanel!=null){
 				charPanel.setVisible(false);
 				charPanel = null;
 			}
-			charPanel = createPanel(players, dataArray);
+			charPanel = createPanel(players);
 			charPanel.setBounds(30, 250, 900, 300);
 			charPanel.setVisible(true);
 			charPanel.updateUI();
@@ -464,25 +599,25 @@ public class PlayerComparePanel extends JPanel {
 		}
 		else{
 			PlayerVO vo1 = blservice.getPlayerAnalysis(playerName1);
-			int assist_1 = vo1.getAssist();
-			int assist_2 = 0;
-			int score_1 = vo1.getScore();
-			int score_2 = 0;
-			int steal_1 = vo1.getSteal();
-			int steal_2 = 0;
-			int miss_1 = vo1.getMiss();
-			int miss_2 = 0;
-			int block_1 = vo1.getBlock();
-			int block_2 = 0;
-			int[] dataArray = {assist_1,assist_2,score_1,score_2,
-					steal_1,steal_2,miss_1,miss_2,block_1,block_1};
+
+			int[] scoreArray = {vo1.getScore(), 0};
+			dataMap.put("得分", scoreArray);
+			int[] reboundArray = {vo1.getTotalRebound(), 0};
+			dataMap.put("篮板", reboundArray);
+			int[] assistArray = {vo1.getAssist(), 0};
+			dataMap.put("助攻", assistArray);
+			int[] freehitArray = {vo1.getFreeHit(), 0};
+			dataMap.put("罚球", freehitArray);
+			int[] stealArray = {vo1.getSteal(), 0};
+			dataMap.put("抢断", stealArray);
+
 			String[] players = {playerName1, playerName2};
 
 			if(charPanel!=null){
 				charPanel.setVisible(false);
 				charPanel = null;
 			}
-			charPanel = createPanel(players, dataArray);
+			charPanel = createPanel(players);
 			charPanel.setBounds(50, 250, 900, 300);
 			charPanel.setVisible(true);
 			charPanel.updateUI();
@@ -524,30 +659,196 @@ public class PlayerComparePanel extends JPanel {
 		BarRenderer renderer = (BarRenderer)categoryplot.getRenderer();
 		renderer.setBaseItemLabelGenerator(new StandardCategoryItemLabelGenerator());
 		renderer.setBaseItemLabelsVisible(true);
-		
+
 		chart.setBackgroundPaint(new Color(0.0F, 0.0F, 0.0F, 0.0F));
 
 		return chart;
 	}
 
-	private static CategoryDataset createDataset(String[] players, int[] dataArray){//创建柱状图数据集
+	private static CategoryDataset createDataset(String[] players){//创建柱状图数据集
 
 		DefaultCategoryDataset dataset = new DefaultCategoryDataset(); 
-		dataset.addValue(dataArray[0], players[0], "助攻"); 
-		dataset.addValue(dataArray[1], players[1], "助攻"); 
-		dataset.addValue(dataArray[2], players[0], "得分"); 
-		dataset.addValue(dataArray[3], players[1], "得分"); 
-		dataset.addValue(dataArray[4], players[0], "抢断"); 
-		dataset.addValue(dataArray[5], players[1], "抢断"); 
-		dataset.addValue(dataArray[6], players[0], "失误"); 
-		dataset.addValue(dataArray[7], players[1], "失误"); 
-		dataset.addValue(dataArray[8], players[0], "盖帽"); 
-		dataset.addValue(dataArray[9], players[1], "盖帽"); 
+
+		for(int i=0;i<checkBoxItem.size();i++){
+			switch(checkBoxItem.get(i)){
+			case "得分":
+				int[] scoreArray = dataMap.get("得分");
+				dataset.addValue(scoreArray[0], players[0], "得分");
+				dataset.addValue(scoreArray[1], players[1], "得分");
+				break;
+			case "篮板":
+				int[] reboundArray = dataMap.get("篮板");
+				dataset.addValue(reboundArray[0], players[0], "篮板");
+				dataset.addValue(reboundArray[1], players[1], "篮板");
+				break;
+			case "助攻":
+				int[] assistArray = dataMap.get("助攻");
+				dataset.addValue(assistArray[0], players[0], "助攻");
+				dataset.addValue(assistArray[1], players[1], "助攻");
+				break;
+			case "罚球":
+				int[] freehitArray = dataMap.get("罚球");
+				dataset.addValue(freehitArray[0], players[0], "罚球");
+				dataset.addValue(freehitArray[1], players[1], "罚球");
+				break;
+			case "抢断":
+				int[] stealArray = dataMap.get("抢断");
+				dataset.addValue(stealArray[0], players[0], "抢断");
+				dataset.addValue(stealArray[1], players[1], "抢断");
+				break;
+			case "盖帽":
+				int[] blockArray = dataMap.get("盖帽");
+				dataset.addValue(blockArray[0], players[0], "盖帽");
+				dataset.addValue(blockArray[1], players[1], "盖帽");
+				break;
+			case "失误":
+				int[] missArray = dataMap.get("失误");
+				dataset.addValue(missArray[0], players[0], "失误");
+				dataset.addValue(missArray[1], players[1], "失误");
+				break;
+			case "犯规":
+				int[] foulArray = dataMap.get("犯规");
+				dataset.addValue(foulArray[0], players[0], "犯规");
+				dataset.addValue(foulArray[1], players[1], "犯规");
+				break;
+			}
+		}
 		return dataset; 
 	}
 
-	private static JPanel createPanel(String[] players, int[] dataArray){
-		JFreeChart chart =createChart(createDataset(players, dataArray));
+	private static JPanel createPanel(String[] players){
+		JFreeChart chart =createChart(createDataset(players));
 		return new ChartPanel(chart); //将chart对象放入Panel面板中去，ChartPanel类已继承Jpanel
 	}
+
+	//根据复选框改变图表内容
+	private void changeTheChart(int checkboxID){
+		switch(checkboxID){
+		case 1:
+			if(CheckBox_1.isSelected()){
+				checkBoxItem.add("得分");
+				checkTheCheckBoxState();
+			}else if(!CheckBox_1.isSelected()){
+				checkBoxItem.remove("得分");
+				checkTheCheckBoxState();
+			}
+			break;
+		case 2:
+			if(CheckBox_2.isSelected()){
+				checkBoxItem.add("篮板");
+				checkTheCheckBoxState();
+			}else if(!CheckBox_2.isSelected()){
+				checkBoxItem.remove("篮板");
+				checkTheCheckBoxState();
+			}
+			break;
+		case 3:
+			if(CheckBox_3.isSelected()){
+				checkBoxItem.add("助攻");
+				checkTheCheckBoxState();
+			}else if(!CheckBox_3.isSelected()){
+				checkBoxItem.remove("助攻");
+				checkTheCheckBoxState();
+			}
+			break;
+		case 4:
+			if(CheckBox_4.isSelected()){
+				checkBoxItem.add("罚球");
+				checkTheCheckBoxState();
+			}else if(!CheckBox_4.isSelected()){
+				checkBoxItem.remove("罚球");
+				checkTheCheckBoxState();
+			}
+			break;
+		case 5:
+			if(CheckBox_5.isSelected()){
+				checkBoxItem.add("抢断");
+				checkTheCheckBoxState();
+			}else if(!CheckBox_5.isSelected()){
+				checkBoxItem.remove("抢断");
+				checkTheCheckBoxState();
+			}
+			break;
+		case 6:
+			if(CheckBox_6.isSelected()){
+				checkBoxItem.add("盖帽");
+				checkTheCheckBoxState();
+			}else if(!CheckBox_6.isSelected()){
+				checkBoxItem.remove("盖帽");
+				checkTheCheckBoxState();
+			}
+			break;
+		case 7:
+			if(CheckBox_7.isSelected()){
+				checkBoxItem.add("失误");
+				checkTheCheckBoxState();
+			}else if(!CheckBox_7.isSelected()){
+				checkBoxItem.remove("失误");
+				checkTheCheckBoxState();
+			}
+			break;
+		case 8:
+			if(CheckBox_8.isSelected()){
+				checkBoxItem.add("犯规");
+				checkTheCheckBoxState();
+			}else if(!CheckBox_8.isSelected()){
+				checkBoxItem.remove("犯规");
+				checkTheCheckBoxState();
+			}
+			break;
+		}
+	}
+	
+	private void checkTheCheckBoxState(){
+		if(checkBoxItem.size()==5){
+			CheckBox_1.setEnabled(false);
+			CheckBox_2.setEnabled(false);
+			CheckBox_3.setEnabled(false);
+			CheckBox_4.setEnabled(false);
+			CheckBox_5.setEnabled(false);
+			CheckBox_6.setEnabled(false);
+			CheckBox_7.setEnabled(false);
+			CheckBox_8.setEnabled(false);
+			for(int i=0;i<checkBoxItem.size();i++){
+				switch(checkBoxItem.get(i)){
+				case "得分":
+					CheckBox_1.setEnabled(true);
+					break;
+				case "篮板":
+					CheckBox_2.setEnabled(true);
+					break;
+				case "助攻":
+					CheckBox_3.setEnabled(true);
+					break;
+				case "罚球":
+					CheckBox_4.setEnabled(true);
+					break;
+				case "抢断":
+					CheckBox_5.setEnabled(true);
+					break;
+				case "盖帽":
+					CheckBox_6.setEnabled(true);
+					break;
+				case "失误":
+					CheckBox_7.setEnabled(true);
+					break;
+				case "犯规":
+					CheckBox_8.setEnabled(true);
+					break;
+				}
+			}
+			
+		}else if(checkBoxItem.size()<5){
+			CheckBox_1.setEnabled(true);
+			CheckBox_2.setEnabled(true);
+			CheckBox_3.setEnabled(true);
+			CheckBox_4.setEnabled(true);
+			CheckBox_5.setEnabled(true);
+			CheckBox_6.setEnabled(true);
+			CheckBox_7.setEnabled(true);
+			CheckBox_8.setEnabled(true);
+		}
+		
+	}
+
 }

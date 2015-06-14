c70743897a63c88264719b4e8e9e725136345e01
tatianguiqu <1013419927@qq.com>
2015-06-14 13:52:37 +0800

    转化图片

--- src/main/java/server/data/Picture.java
+++ src/main/java/server/data/Picture.java
@@ -14,19 +14,60 @@ import org.apache.batik.transcoder.TranscoderInput;
 import org.apache.batik.transcoder.TranscoderOutput;
 import org.apache.batik.transcoder.image.PNGTranscoder;
 
-public  final class Picture {
-	public static  ImageIcon getTeamPic(String abbreviation) {
-		File file = new File("./conf/transferStation/transferPic.png");
-		FileOutputStream outputStream = null;
-		try {
-			outputStream = new FileOutputStream(file);
-			convertToPng(getSYGCode(abbreviation), outputStream);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		ImageIcon image = new ImageIcon("./conf/transferStation/transferPic.png");
+import server.po.MatchPO;
+import console.Console;
+
+public final class Picture {
+//	public static void main(String[] args) throws Exception {
+//		getPNG();
+//	}
+	public static ImageIcon getTeamPic(String abbreviation) {
+//		File file = new File("./conf/transferStation/transferPic.png");
+//		FileOutputStream outputStream = null;
+//		try {
+//			outputStream = new FileOutputStream(file);
+//			convertToPng(getSYGCode(abbreviation), outputStream);
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		ImageIcon image = new ImageIcon(
+				"./conf/transferStation/"+abbreviation+".png");
 		return image;
+	}
+
+	private static void getPNG() {
+
+		File f = new File(Console.path + "/teams");
+		File[] matchesFile = f.listFiles();
+		for (File i : matchesFile) {
+			String[] name = i.getName().split("\\.");
+			if (name.length == 2) {
+				String code = "";
+				try {
+					BufferedReader br = new BufferedReader(new FileReader(i));
+					String line = "";
+					while ((line = br.readLine()) != null) {
+						code = code + line + "\r\n";
+						;
+					}
+					br.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				String abbreviation = name[0];
+				File file = new File("./conf/transferStation/"+abbreviation+".png");
+				FileOutputStream outputStream = null;
+				try {
+					outputStream = new FileOutputStream(file);
+					convertToPng(code, outputStream);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+
 	}
 
 	static private String getSYGCode(String abbreviation) {
@@ -58,11 +99,13 @@ public  final class Picture {
 	}
 
 	public static ImageIcon getPlayerAction(String name) {
+		name=name.replaceAll("\\d", "");
 		ImageIcon image = new ImageIcon("nba/players/action/" + name + ".png");
 		return image;
 	}
 
 	public static ImageIcon getPlayerPortrait(String name) {
+		name=name.replaceAll("\\d", "");
 		ImageIcon image = new ImageIcon("nba/players/portrait/" + name + ".png");
 		return image;
 	}
